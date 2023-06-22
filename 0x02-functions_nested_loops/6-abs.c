46728cc752d7b418bcc68cc4c730a116e86108e5
Michael Otu <mcotu08@gmail.com/>
2023-06-22 08:10:38 -0700

    Absolute numbers and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - checks for the absolute number of an integer
+ *
+ * @int n: is the integer
+ *
+ * Return: always -n if true n if false
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return -n;
+	else
+		return n;
+}

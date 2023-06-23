e9e8611ea3229b09ee4e46011ecbbaaf54d46878
Michael Otu <mcotu08@gmail.com/>
2023-06-23 04:19:58 -0700

    Arithmetic operator: Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies variables
+ * @a: first parameter name
+ * @b: second parameter name
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

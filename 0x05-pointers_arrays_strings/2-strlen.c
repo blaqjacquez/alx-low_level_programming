ea97ba74bd33c3d2967c1b960e94c3931389cf85
Michael Otu <mcotu08@gmail.com/>
2023-06-27 16:33:39 -0700

    String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - shows string lenght
+ * @s: parameter name
+ * Return: Always 0 (success)
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+}

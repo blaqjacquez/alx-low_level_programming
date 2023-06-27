e8628c7be53a6b25c6e2a1e46e42d843c668e9ea
Michael Otu <mcotu08@gmail.com/>
2023-06-27 16:50:56 -0700

    Put a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts - prints a string and a new line
+ * @str: parameter name
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
